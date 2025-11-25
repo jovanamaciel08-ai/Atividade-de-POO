2971e79b082440231b2c884ce36f07da65148e68
Caio Csermak <jovana.maciel08@gmail.com>
2025-11-25 09:46:44 -0300

    Atualizando e organizando os códigos

--- caixa.py
+++ caixa.py
@@ -1,16 +1,10 @@
 class Caixa:
     def __init__(self):
-        self.__produtos = []  
-        self.__total = 0      
+        self.produtos = []
 
-    
+    def adicionar_produto(self, nome, preco, quantidade):
+        self.produtos.append({"nome": nome, "preco": preco, "quantidade": quantidade})
+
     def get_total(self):
-        return self.__total   
-
-    def adicionar_produto(self, nome, preco):
-        if preco >= 0:
-            self.__produtos.append((nome, preco))  
-            self.__total += preco                 
-            print(f"Produto {nome}.\nPreço: {preco:.2f}.\nTotal agora: {self.__total:.2f}")
-        else:
-            print("O preço do produto deve ser positivo.")
+        total = sum(p["preco"] * p["quantidade"] for p in self.produtos)
+        return total
